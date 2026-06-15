6524a587ef83e56122b9919248bf84a7b09884cd
Xavier1500 <xavier.donovan@hgs.hiddengeniusproject.org>
2026-06-15 14:54:11 -0700

    uploaded practice project

---
+++ practice project A/practice project A/practice_project_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_project_AApp.swift
+//  practice project A
+//
+//  Created by Xavier Donovan on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct practice_project_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
